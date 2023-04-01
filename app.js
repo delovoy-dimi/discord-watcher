adddf505cfa76d9cc7e9d11a5e7c3b93666efc1a
delovoy-dimi <dimi.delovoydao@gmail.com>
2023-04-01 19:06:31 +1000

    monitor multiple channels

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 import { setTgBotEvents, logException } from './tg-bot-setup.js'
 import { startChannelMonitoring } from './discord-setup.js'
+
+const discordSettings = {
+  channelOne: {
+    discordToken: 'qwe',
+    channelId: 'qwe',
+    requestDelay: 5000,
+    context: { lastReportedMsgId: 0, msgContentHash: {} },
+  },
+  channelTwo: {
+    discordToken: 'qwe',
+    channelId: 'qwe',
+    requestDelay: 5000,
+    context: { lastReportedMsgId: 0, msgContentHash: {} },
+  },
+}
 ;(async () => {
   try {
     setTgBotEvents()
-    await startChannelMonitoring()
+    startChannelMonitoring(discordSettings.channelOne)
+    startChannelMonitoring(discordSettings.channelTwo)
   } catch (e) {
     logException(`Ooops: ${e.message}`)
   }
--- discord-setup.js
+++ discord-setup.js
@@ -2,21 +2,14 @@ import axios from 'axios'
 import { logException, notifySubscribers } from './tg-bot-setup.js'
 import './loadEnv.js'
 
-const discordToken = process.env.DISCORD_ACCESS_TOKEN
-const channelId = process.env.DISCORD_CHANNEL_ID
-const requestDelay = process.env.DISCORD_CHANNEL_PROBE_DELAY
-
-let lastReportedMsgId = 0
-const msgContentHash = {}
-
-export const startChannelMonitoring = async () => {
+export const startChannelMonitoring = async (settings) => {
   while (true) {
     try {
-      const messages = await getMessagesToReport()
+      const messages = await getMessagesToReport(settings)
       const formattedMessage = formatMessage(messages)
       if (formattedMessage) await notifySubscribers(formattedMessage)
 
-      await delay(requestDelay)
+      await delay(settings.requestDelay)
     } catch (e) {
       logException(`ChannelMonitoring failed with ${e.message}`)
     }
@@ -25,13 +18,13 @@ export const startChannelMonitoring = async () => {
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
-const getLatestDiscordChanelMessages = async (latestCount) => {
+const getLatestDiscordChanelMessages = async (settings, latestCount) => {
   try {
-    var url = `https://discord.com/api/v9/channels/${channelId}/messages?limit=${latestCount}`
+    var url = `https://discord.com/api/v9/channels/${settings.channelId}/messages?limit=${latestCount}`
     return (
       await axios.get(url, {
         headers: {
-          authorization: discordToken,
+          authorization: settings.discordToken,
         },
       })
     ).data
@@ -41,16 +34,20 @@ const getLatestDiscordChanelMessages = async (latestCount) => {
   }
 }
 
-const filterNotReportedMessages = (messages) => {
-  const messagesToReport = messages.filter((m) => m.id > lastReportedMsgId || hashCode(m.content) != msgContentHash[m.id])
-  lastReportedMsgId = messagesToReport[0]?.id ? Math.max(messagesToReport[0]?.id, lastReportedMsgId) : lastReportedMsgId
-  messages.forEach((m) => (msgContentHash[m.id] = hashCode(m.content)))
+const filterNotReportedMessages = (settings, messages) => {
+  const messagesToReport = messages.filter(
+    (m) => m.id > settings.context.lastReportedMsgId || hashCode(m.content) != settings.context.msgContentHash[m.id]
+  )
+  settings.context.lastReportedMsgId = messagesToReport[0]?.id
+    ? Math.max(messagesToReport[0]?.id, settings.context.lastReportedMsgId)
+    : settings.context.lastReportedMsgId
+  messages.forEach((m) => (settings.context.msgContentHash[m.id] = hashCode(m.content)))
   return messagesToReport
 }
 
-const getMessagesToReport = async () => {
-  const latestMessages = await getLatestDiscordChanelMessages(5)
-  return filterNotReportedMessages(latestMessages)
+const getMessagesToReport = async (settings) => {
+  const latestMessages = await getLatestDiscordChanelMessages(settings, 5)
+  return filterNotReportedMessages(settings, latestMessages)
 }
 
 const formatMessage = (messages) => {
